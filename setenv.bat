bece5a9a1815def96c15abfbcb1a7218575fdc77
JAIN, PIYUSH <e0267549@u.nus.edu>
2018-09-17 14:23:55 +0800

    Changes to SetEnv to Run Project

--- setenv.bat
+++ setenv.bat
@@ -1,6 +1,4 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
-set ANT_HOME=..\apache-ant-1.6.5
-set JUNIT_HOME=..\junit3.8.2
+set JUNIT_HOME=C:\NUS 2nd Sem\Software Quality Management\Software Configuration Management
 
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
 set CLASSPATH=%JUNIT_HOME%\junit.jar
